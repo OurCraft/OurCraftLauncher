bbab4876ff82ad1509dd854253e6f073bb6c662d
iLexiconn <ilexiconn@gmail.com>
2014-10-08 17:51:21 +0200

    Hehe

--- src/main/java/org/craft/launch/gui/GuiBackground.java
+++ src/main/java/org/craft/launch/gui/GuiBackground.java
@@ -6,25 +6,33 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.Random;
 
 public class GuiBackground extends JPanel
 {
     private float progress = 0.0f;
     private int animationID = -1;
 
-    public Image background;
+    public Random random;
+    public Image[] backgrounds;
+    public Image logo;
+    public int imageID;
 
     public GuiBackground() throws IOException
     {
-        background = ImageIO.read(GuiBackground.class.getResourceAsStream("/background.png"));
+        random = new Random();
+        backgrounds = new Image[1];
+
+        logo = ImageIO.read(GuiBackground.class.getResourceAsStream("/logo.png"));
+        for (int i = 0; i < backgrounds.length; i++) backgrounds[i] = ImageIO.read(GuiBackground.class.getResourceAsStream("/background" + i + ".png"));
+        imageID = random.nextInt(backgrounds.length);
     }
 
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
-        g.drawImage(background, 0, 0, background.getWidth(null), background.getHeight(null) * ((int) (getProgress(0) * 100)) / 100, null);
-
-        updateUI();
+        g.drawImage(backgrounds[imageID], 0, 0, getWidth(), getHeight(), null);
+        g.drawImage(logo, 0, 0, logo.getWidth(null), logo.getHeight(null) * ((int) (getProgress(0) * 100)) / 100, null);
     }
 
     public void animate(int id, int time)
